8481155592fae7ddc4415f5a4441597037b5e93b
Felipe Corrêa <felipe.correa2@outlook.com>
2025-11-24 20:09:28 -0300

    Consumo da api do github

--- src/busca/MainBusca.java
+++ src/busca/MainBusca.java
@@ -8,6 +8,13 @@
 
 package busca;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class MainBusca {
@@ -20,6 +27,31 @@ public class MainBusca {
         System.out.print("\nDigite um nome de usuário:");
         String nomeParaBusca = scanner.next();
 
+        String endereco = "https://api.github.com/users/" + nomeParaBusca;
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .header("Accept", "application/vnd.github.v3+json")
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request , HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 404) {
+                throw new ErroConsultaGitHubException("Usuário não encontrado no GitHub.");
+            }
+
+            String json = response.body();
+            System.out.println(json);
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Opss… Houve um erro durante a consulta à API do GitHub.");
+            e.printStackTrace();
+        } catch (ErroConsultaGitHubException e) {
+            System.out.println(e.getMessage());
+        }
 
 
     }
--- src/senha/SenhaInvalidaException.java
+++ src/senha/SenhaInvalidaException.java
@@ -1,9 +1,6 @@
 package senha;
 
 public class SenhaInvalidaException extends RuntimeException {
-
-    private String message;
-
     public SenhaInvalidaException(String message) {
         super(message);
     }
